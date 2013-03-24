4d08bc3dbc1f2fe41e8722c41c1d98488eaa6bbb
Andy Fundinger <andriod@gmail.com>
2013-03-24 15:28:57 -0400

    Added copyright notice and GPL3 license.

--- clock.py
+++ clock.py
@@ -1,3 +1,5 @@
+#Copyright 2013 Andy Fundinger
+
 from ConfigParser import SafeConfigParser
 from datetime import datetime
 from time import sleep
--- clock_enhanced.py
+++ clock_enhanced.py
@@ -1,3 +1,5 @@
+#Copyright 2013 Andy Fundinger
+
 from datetime import datetime
 from time import sleep
 import re
